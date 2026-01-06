f72724b8e6b882656b4d2b2698a8f1e922e6ca5a
Zhenya Nikiforov <12A92@mail.ru>
2026-01-06 14:11:28 +0500

    Второе задание, разные файлы, доп. тесты

--- 2_Catch2/out/build/x64-Debug/Catch2-devel/cmake_install.cmake
+++ 2_Catch2/out/build/x64-Debug/Catch2-devel/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Женя/Desktop/2_Catch2/Catch2-devel
+# Install script for directory: C:/Users/Женя/Desktop/СамоПроверка/Тесты_катч2/Testirovanie_Catch2/2_Catch2/Catch2-devel
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/Женя/Desktop/2_Catch2/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/Женя/Desktop/СамоПроверка/Тесты_катч2/Testirovanie_Catch2/2_Catch2/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,12 +34,12 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/Женя/Desktop/2_Catch2/out/build/x64-Debug/Catch2-devel/src/cmake_install.cmake")
+  include("C:/Users/Женя/Desktop/СамоПроверка/Тесты_катч2/Testirovanie_Catch2/2_Catch2/out/build/x64-Debug/Catch2-devel/src/cmake_install.cmake")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/Женя/Desktop/2_Catch2/out/build/x64-Debug/Catch2-devel/install_local_manifest.txt"
+  file(WRITE "C:/Users/Женя/Desktop/СамоПроверка/Тесты_катч2/Testirovanie_Catch2/2_Catch2/out/build/x64-Debug/Catch2-devel/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- 2_Catch2/out/build/x64-Debug/cmake_install.cmake
+++ 2_Catch2/out/build/x64-Debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Женя/Desktop/2_Catch2
+# Install script for directory: C:/Users/Женя/Desktop/СамоПроверка/Тесты_катч2/Testirovanie_Catch2/2_Catch2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/Женя/Desktop/2_Catch2/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/Женя/Desktop/СамоПроверка/Тесты_катч2/Testirovanie_Catch2/2_Catch2/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,14 +34,14 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/Женя/Desktop/2_Catch2/out/build/x64-Debug/Catch2-devel/cmake_install.cmake")
+  include("C:/Users/Женя/Desktop/СамоПроверка/Тесты_катч2/Testirovanie_Catch2/2_Catch2/out/build/x64-Debug/Catch2-devel/cmake_install.cmake")
 
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/Женя/Desktop/2_Catch2/out/build/x64-Debug/install_local_manifest.txt"
+  file(WRITE "C:/Users/Женя/Desktop/СамоПроверка/Тесты_катч2/Testirovanie_Catch2/2_Catch2/out/build/x64-Debug/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -57,6 +57,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/Женя/Desktop/2_Catch2/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/Женя/Desktop/СамоПроверка/Тесты_катч2/Testirovanie_Catch2/2_Catch2/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
